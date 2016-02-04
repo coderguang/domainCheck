f3f920ab37cd77b2fa5377f57b9b2ed68e4f8909
static service <royalchen@royalchen.com>
2016-02-04 16:38:10 +0800

    deleted:    cn/f3.sh 	deleted:    cn/f4.sh 	deleted:    cn/file 	deleted:    cn/file.cpp 	deleted:    cn/start.sh 	deleted:    com/file 	deleted:    com/file.cpp 	deleted:    com/find.sh 	deleted:    com/list3.txt 	deleted:    com/list4.txt 	deleted:    com/makefile 	deleted:    com/numlist3.txt 	deleted:    com/numlist4.txt 	deleted:    com/numlist5.txt 	deleted:    com/start.sh 	deleted:    start.sh

--- cn/f4.sh
+++
@@ -1,9 +0,0 @@
-#~/bin/bash
-echo "f4 start time is "
-date
-while read line
-do
-	pwhois $line |grep "No match"
-done<list4.txt
-echo "f4 end time is "
-date
--- com/start.sh
+++
@@ -1,5 +0,0 @@
-#~bin/bash
-./f3.sh>log3 &
-./f4.sh>log4 &
-
-echo "start success"
